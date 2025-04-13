2e9a3940a97195dc09bb74db584307411ab41544
PompayPl <149879671+PompayPl@users.noreply.github.com>
2025-04-13 21:50:06 +0800

    Update settings.py

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zv&z3_op*p*lhpeb)7b@95!=*dlh2shjg9+kr5y_^eb2mw^8ya
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -126,4 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media'
